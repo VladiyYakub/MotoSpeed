27118448237df126063b5f87cf387628a7e0fb49
Vladyslav Yakubenko <vladikyakubenko@gmail.com>
2023-02-17 15:07:44 +0200

    Initial commit

---
+++ Assets/Game/Scripts/CameraFollow.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform Target;
+    private Vector3 _offset;
+
+    private void Start()
+    {
+        _offset = transform.position - Target.position;
+    }
+    private void Update()
+    {
+        transform.position = new Vector3(Target.position.x + _offset.x,0, Target.position.z + _offset.z);
+    }
+}
